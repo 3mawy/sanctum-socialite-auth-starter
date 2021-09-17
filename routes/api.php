6e94e2943a57c02432d68d7fa4c3b42f0961e32e
3mawy <m.3mawyy@gmail.com>
2021-09-17 04:37:18 +0200

    added auth routes

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\Auth\SocialAuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Route::post('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('login/{provider}', [SocialAuthController::class, 'redirectToProvider']);
+Route::get('login/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']);
